05a28539911dfcaf5bf504f6b7eaceaea67c3157
YuliyaNY <40675889+YuliyaNY@users.noreply.github.com>
2019-07-26 15:16:02 +0300

    Update main.js

--- Lesson 12/main.js
+++ Lesson 12/main.js
@@ -1,42 +1,62 @@
-// Задание 1:
+// Задание 1: Преобразование массива имен в объекты.
 
-var names = ['Vasya', 'Kate', 'Olga'];
-var obj = {};
-var nameObj = names.map(function(name) {
-    obj['name'] = names;
-    return obj;
-});
-
-
-// Задание 2:
-
-function toStr(arr) {
-    console.log('Текущее время ' + arr.join(':'));
+function mapNamesArr(names) {
+	return names.map(function(name) {
+		return {name: name};
+	});
 }
 
-toStr(['00', '13', '24']);
+mapNamesArr(['Vasya', 'Kate', 'John']);
 
 
-// Задание 3:
+// Задание 2: Получение строки со временем.
 
-function vowelCount(str) {
-    var count = 0;
+function getTime(arr) {
+	return arr.reduce(function(prevVal, currentVal) {
+		return prevVal + ' : ' + currentVal;
+	}, 'Текущее время');
+}
 
-    for (var i = 0; i < str.length; i++) {
+getTime(['00', '13', '24']);
 
-        if (str[i].toLowerCase() == 'а' || str[i].toLowerCase() == 'о' || str[i].toLowerCase() == 'и'
-            || str[i].toLowerCase() == 'е' || str[i].toLowerCase() == 'ё' || str[i].toLowerCase() == 'э'
-            || str[i].toLowerCase() == 'ы' || str[i].toLowerCase() == 'у' || str[i].toLowerCase() == 'ю'
-            || str[i].toLowerCase() == 'я') {
 
-            count+=1;
+// Задание 3: Подсчет кол-ва гласных в тексте (вариант 1).
+
+function countVowelLetters(text) {
+    text = text.toLowerCase();
+
+    var vowelLetters = 'аяыиоёуюэеaeiouy',
+        counter = 0;
+
+    for (var i = 0; i < vowelLetters.length; i++) {
+        for (var j = 0; j < text.length; j++) {
+            vowelLetters[i] === text[j] && counter++;
         }
     }
-    return count;
-}
-vowelCount('Доброе утро');
 
-// другой вариант решения:
+    return counter;
+}
+
+countVowelLetters('Шла Саша по шоссе И сосала сУшку'); // 12
+
+// Подсчет кол-ва гласных в тексте (вариант 2).
+
+function countVowelLetters(text) {
+    text = text.toLowerCase().split('');
+
+    var vowelLetters = ['а', 'я', 'ы', 'и', 'о', 'ё', 'у', 'ю', 'э', 'е', 'a', 'e', 'i', 'o', 'u', 'y'],
+        counter = 0;
+
+    text.forEach(function(letter) {
+        vowelLetters.indexOf(letter) !== -1 && counter++;
+    });
+
+    return counter;
+}
+
+countVowelLetters('Шла Саша по шоссе И сосала сУшку'); // 12
+
+// через регулярные выражния.
 
 function vowelCount(str) {
 
@@ -46,10 +66,70 @@ function vowelCount(str) {
 console.log(vowelCount('Доброе утро'));
 
 
-// Задание 4: ???
+// Задание 4: Подсчет количества букв в предложениях.
 
-function countLetters(txt) {
-    return txt + ' ' + txt.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split('').join('').length;
+function countSentencesLetters(text) {
+	var sentences = text.split(/[.!?]+/);
+	
+	if (!(sentences[sentences.length - 1].length)) {
+		sentences.pop();
+	}
 
+	sentences.forEach(function(sentence) {
+        console.log(sentence.trim() + ': Letters quantity is: ' + sentence.split(/[,\s]+/).join('').length);
+	});
 }
-countLetters('Hello World!!!');
+
+countSentencesLetters('Привет, Маша! Маша. Как дела, Маша?');
+
+// Задание 5: Поиск максимально повторяющегося слова в тексте (вариант 1).
+
+function findFrequentWord(story) {
+	var storyWords = story.split(/[.,!?\s]+/),
+		wordsArr = [];
+
+	storyWords.forEach(function(initialWord) {
+        var quantity = 0;
+
+        storyWords.forEach(function(comparedWord) {
+			(initialWord === comparedWord) && quantity++;
+		});
+
+        wordsArr.push({word: initialWord, quantity: quantity});
+    });
+	
+	wordsArr.sort(function(a, b) {
+		return b.quantity - a.quantity;
+	});
+	
+	var firstWord = wordsArr[0];
+	
+	return 'Максимальное число повторений у слова \"' + firstWord.word + '\" - ' + firstWord.quantity + '.';
+}
+
+findFrequentWord('Привет, Маша! Маша... Как дела, Маша?');
+
+// Поиск максимально повторяющегося слова в тексте (вариант 2).
+
+function findFrequentWord(story) {
+	var storyWords = story.split(/[.,!?\s]+/),
+		repeatCounter = 0,
+ 		repeatWord = '';
+
+	storyWords.forEach(function(initialWord) {
+        var counter = 0;
+
+        storyWords.forEach(function(comparedWord) {
+            (initialWord === comparedWord) && counter++;
+        });
+
+        if (counter > repeatCounter) {
+            repeatCounter = counter;
+            repeatWord = initialWord;
+        }
+	});
+	
+	return 'Максимальное число повторений у слова \"' + repeatWord + '\" - ' + repeatCounter + '.';
+}
+
+findFrequentWord('Привет, Маша! Маша... Как дела, Маша?');
