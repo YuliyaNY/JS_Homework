ee3a15ba6d31a0500d94cf245d24e81bdc8ff421
YuliyaNY <40675889+YuliyaNY@users.noreply.github.com>
2019-08-03 13:08:53 +0300

    Update main.js

--- Lesson 13/Part 2/main.js
+++ Lesson 13/Part 2/main.js
@@ -1,79 +1,92 @@
-var x = document.getElementById("x");
-var y = document.getElementById("y");
-var button = document.getElementById("button");
+var xInput = document.getElementById('x'),
+    yInput = document.getElementById('y'),
+    createBtn = document.getElementsByTagName('button')[0],
+	body = document.body;
 
-function success() {
-	 if(x.value==="" || y.value==="") { 
-            button.disabled = true; 
-        } else { 
-           button.disabled = false;
-        }
+xInput.onkeyup = disableBtn;
+yInput.onkeyup = disableBtn;
+
+function disableBtn() {
+    createBtn.disabled = !(xInput.value.trim() && yInput.value.trim());
+}
+
+createBtn.onclick = function() {
+    var xValue = +xInput.value,
+        yValue = +yInput.value;
+
+    if (!isValidFieldValue(xValue)) {
+        resetForm([xInput]);
+        showError(xInput);
     }
 
-//2: validation 
-
-function isNumb() {
-  x = document.getElementById('x').value;
-  y  = document.getElementById('y').value;
-
-  if (isNaN(x) || x < 1 || x > 10 || y < 1 || y > 10) {
-    alert('Input not valid');
-  } else  {
-    chessBoard();
-  }
-}
-
-//3: chess board
-
-function chessBoard() {
-var x = document.getElementById('x').value;
-var y = document.getElementById('x').value;
-
-var table = document.createElement('table');
-for (var i = 1; i <= x; i++) {
-    var tr = document.createElement('tr');
-    for (var j = 1; j <= y; j++) {
-        var td = document.createElement('td');
-        if (i%2 == j%2) {
-            td.className = 'white';
-        } else {
-            td.className = 'black';
-        }
-        tr.appendChild(td);
+    if (!isValidFieldValue(yValue)) {
+        resetForm([yInput]);
+        showError(yInput);
     }
-    table.appendChild(tr);
-}
-document.body.appendChild(table);
 
-var i = 0;
-  for (i = 0; i < document.getElementsByClassName('black').length; i++) {
-    document.getElementsByClassName('black')[i].style.backgroundColor = 'black';
-  }
-  for (i = 0; i < document.getElementsByClassName('white').length; i++) {
-    document.getElementsByClassName('white')[i].style.backgroundColor = 'white';
-  }
-  for (i = 0; i < document.getElementsByTagName('td').length; i++) {
-    document.getElementsByTagName('td')[i].style.width = '25px';
-    document.getElementsByTagName('td')[i].style.height = '25px';
-  }
-}
-
-// change color ???
-
-function start() {
-  var submit = document.getElementsByTagName('td');
-  submit.addEventListener('click', toggle);
+    if (isValidFieldValue(xValue) && isValidFieldValue(yValue)) {
+        resetForm([xInput, yInput]);
+        drawChessTable(xValue, yValue);
+    }
 };
 
-function toggle() {
-  var color = document.getElementsByClassName('white');
-    if(color.style.backgroundColor = 'white') {
-      color.style.backgroundColor ='black';
-   	} else {
-    color.style.backgroundColor = "white";
+function isValidFieldValue(value) {
+    return value && isInteger(value) && value >=1 && value <= 10;
+}
+
+function isInteger(value) {
+    return value === parseInt(value);
+}
+
+function showError(input) {
+    alert('Введите корректное значение в поле ' + input.id.toUpperCase() + ' - целое число от 1 до 10.');
+}
+
+function resetForm(inputs) {
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].value = '';
+    }
+
+    disableBtn();
+}
+
+function drawChessTable(xValue, yValue) {
+    var table = document.getElementsByTagName('table')[0];
+
+    if (table) {
+        table.remove();
+    }
+
+    table = document.createElement('table');
+
+    for (var i = 0; i < yValue; i++) {
+        var tr = document.createElement('tr');
+
+        for (var j = 0; j < xValue; j++) {
+            var td = document.createElement('td');
+
+            !((i + j) % 2) && td.classList.add('black');
+
+            tr.appendChild(td);
+        }
+
+        table.appendChild(tr);
+    }
+
+    body.appendChild(table);
+}
+
+body.onclick = function(event) {
+	var target = event.target;
+
+	if (target.tagName === 'TD') {
+		var tds = document.getElementsByTagName('td');
+
+		for (var i = 0; i < tds.length; i++) {
+			tds[i].classList.toggle('black');
+		}
 	}
 };
-start();
 
 
 
